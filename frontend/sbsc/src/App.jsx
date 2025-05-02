5dd6a2c7a512cdc830ec5b8013912330b9db0ea2
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-05-02 22:05:11 +0530

    modified to full width

--- frontend/sbsc/src/App.jsx
+++ frontend/sbsc/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './App.css'
 
 function App() {
   return (
