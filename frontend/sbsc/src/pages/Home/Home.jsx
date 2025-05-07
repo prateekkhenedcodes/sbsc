1a946d5ebc238c70b8621aea781f1c24b12f2a23
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-05-07 17:20:28 +0530

    added the mostliked image to the home page

--- frontend/sbsc/src/pages/Home/Home.jsx
+++ frontend/sbsc/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
+import top1Img from '../../assets/cutie.png'; // Replace with your actual image path
 
 export default function Home() {
   return (
@@ -7,12 +8,20 @@ export default function Home() {
       <div className="sub-header">
         <span className="sub-text">DAILY COMICS, WEEKLY ANIMATIONS & MORE!</span>
         <div className="social-icons">
-            <a href="#"><i className="fab fa-facebook"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-youtube"></i></a>
-         </div>
-    </div>
+          <a href="#"><i className="fab fa-facebook"></i></a>
+          <a href="#"><i className="fab fa-instagram"></i></a>
+          <a href="#"><i className="fab fa-twitter"></i></a>
+          <a href="#"><i className="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+      <div className="top-container">
+        <div className="top-image-wrapper">
+          <img src={top1Img} alt="Top 1 Contestant" className="top-image" />
+        </div>
+        <h2 className="top-label">Top 1 in the Contest</h2>
+      </div>
+
       <div className="hero-content">
         <h1>Wassup!!!!</h1>
         <p>The internet's most brutally honest appearance rating platform</p>
